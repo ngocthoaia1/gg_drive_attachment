bcaba42dc341fc913c1e8a8a79f1b9d0643b12b5
Ngoc Thoai <ngocthoaia1@gmail.com>
2021-08-01 16:25:15 +0700

    support retry

--- test/gg_drive_attachment_test.rb
+++ test/gg_drive_attachment_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
+# ./bin/rails test test/gg_drive_attachment_test.rb
+
 class GgDriveAttachment::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, GgDriveAttachment
+  test "can upload file" do
+    file = OpenStruct.new(
+      original_filename: "test_#{Time.current}",
+      path: Rails.root.join("public/404.html").to_s
+    )
+    attachment = GgDriveAttachment::Attachment.new(file:file)
+    assert(attachment.save)
   end
 end
